9250d75c4cad0c0ffa40ca0a41f8de25bcd4fd23
Roth Conrad <rotheconrad@gatech.edu>
2020-02-29 09:26:26 -0500

    Add file format tests to renaming scripts

--- 01a_Fastq_rename_sequences.py
+++ 01a_Fastq_rename_sequences.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
-''' Renames sequences in a fastq file
+''' Renames sequences in a fastq formatted file.
 
 Magic Blast cuts the read name at the first space character and
 reports the name before the space as the query ID.
 
-However, Fastq files can be named as:
+However, fastq files can be named as:
     @D00468:261:HYTMHBCX2:1:1101:9119:31637 1:N:0:CAGAGAGG+ACTGCATA
     @D00468:261:HYTMHBCX2:1:1101:9119:31637 2:N:0:CAGAGAGG+ACTGCATA
 where the unique identifier can come after the space chacter.
 
-Filtering for best hits and retrieving the fasta sequence of a magic
-blast match becomes impossible once the unique identifier is lost.
+Filtering for best hits and retrieving the nucleotide sequence of a magic
+blast match becomes much more involved once the unique identifier is lost.
 
 This script renames the fastq sequences with a simple name:
 @Read_0000000000001
 @Read_0000000000002
 etc...
+
+!!! CAUTION !!! Renames file in place (ie: overwrites the input file) !!!
 
 -------------------------------------------
 Author :: Roth Conrad
@@ -29,21 +31,29 @@ All rights reserved
 -------------------------------------------
 '''
 
-import argparse, subprocess
+import argparse, sys, subprocess
 
 def read_fastq(fq):
-    linecount = 0
+    #linecount = 0
     while fq:
-        name = fq.readline().rstrip().split(' ')[0][1:]
+        # fastq format line 1x is read name
+        name = fq.readline().rstrip()
+        # Check for end of file and break.
         if not name: break
+        # fastq format line 2x is nucleotide sequence
         seq = fq.readline().rstrip()
+        # fastq format line 3x is wierd blank '+'' line
         blank = fq.readline().rstrip()
+        # fastq format line 4x is the quality score line
         qal = fq.readline().rstrip()
-        linecount += 4
-        if linecount % 4 == 0: yield (name, seq, blank, qal)
+        # Return these four lines each iteration
+        yield (name, seq, blank, qal)
+
+        #linecount += 4
+        #if linecount % 4 == 0: yield (name, seq, blank, qal)
 
 def main():
-        # Configure Argument Parser
+    # Configure Argument Parser
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter
@@ -65,16 +75,30 @@ def main():
     args=vars(parser.parse_args())
 
     infile = args['fastq_input_file']
+    # define temporary file name.
     outfile = args['fastq_input_file'].split('.')[0] + '.temp'
     prefix = args['prefix']
+
+    # open the file to check fastq format
+    with open(infile, 'r') as file:
+        # only need to read first line
+        line = file.readline()
+        # Verify fastq format or exit with message
+        if not line.startswith('@'):
+            print('\n\nError reading fastq formatted read names!\n\n')
+            sys.exit('\n\nAre you sure this is a fastq formatted file?\n\n')
+
+    # initialize read count for new read names.
     readcount = 0
-
+    # Read through the input fastq file, change read names, and write new file.
     with open(infile, 'r') as f, open (outfile, 'w') as o:
-
+        # Iterate through fastq format with read_fastq function
         for name, seq, blank, qal in read_fastq(f):
+            # increment readcount and write file with numbered read names.
             readcount += 1
             o.write(f'@{prefix}_{readcount:013}\n{seq}\n{blank}\n{qal}\n')
 
+    # Move the temporary file to overwrite the orignal file.
     _ = subprocess.run(['mv', outfile, infile])
 
 if __name__ == "__main__":
--- 01b_Fasta_rename_sequences.py
+++ 01b_Fasta_rename_sequences.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 
-''' Rename fasta sequences
+''' Renames sequences in a fasta formatted file.
 
-Reads fasta file and replaces sequence names with >prefix_i where 
-i = 1 through the number of sequences in the fasta file.
+Magic Blast cuts the read name at the first space character and
+reports the name before the space as the query ID.
 
-Renames file in place (ie: overwrites the input file).
+However, fasta files can be named as:
+    >D00468:261:HYTMHBCX2:1:1101:9119:31637 1:N:0:CAGAGAGG+ACTGCATA
+    >D00468:261:HYTMHBCX2:1:1101:9119:31637 2:N:0:CAGAGAGG+ACTGCATA
+where the unique identifier can come after the space chacter.
+
+Filtering for best hits and retrieving the nucleotide sequence of a magic
+blast match becomes much more involved once the unique identifier is lost.
+
+This script renames the fasta sequences with a simple name:
+>Read_0000000000001
+>Read_0000000000002
+etc...
+
+!!! CAUTION !!! Renames file in place (ie: overwrites the input file) !!!
 
 -------------------------------------------
 Author :: Roth Conrad
@@ -18,30 +31,44 @@ All rights reserved
 -------------------------------------------
 '''
 
-import argparse, subprocess
+import argparse, sys, subprocess
 
 def read_fasta(fp):
+    # initialize name, seq objects
     name, seq = None, []
+    # iterate through lines in file
     for line in fp:
+        # strip new line characters
         line = line.rstrip()
+        # check if line is read name
         if line.startswith(">"):
+            # return prior name, seq object at each new name
             if name: yield (name, ''.join(seq))
+            # define new name, empty seq object for next sequence
             name, seq = line, []
+        # else it is sequence so add to current seq object
         else:
             seq.append(line)
+    # this returns the last name, seq pair of the file.
     if name: yield (name, ''.join(seq))
 
 def Fasta_rename_sequences(infile, prefix):
 
+    # define temporary file name.
     outfile = prefix + '.temp'
 
+    # open the infile and temp file: read and write.
     with open(infile, 'r+') as f, open(outfile, 'w') as o:
+        # initialize read count for new read names.
         i = 1
+        # Iterate through the fasta format with read_fasta function.
         for name, seq in read_fasta(f):
-            newName = f'>{prefix}_{i:013}\n{seq}\n'
-            o.write(newName)
+            # Write temp file with numbered read names.
+            o.write(f'>{prefix}_{i:013}\n{seq}\n')
+            # increment read count
             i += 1
 
+    # Move the temp file to replace the original file.
     _ = subprocess.run(['mv', outfile, infile])
 
 def main():
@@ -67,8 +94,16 @@ def main():
         )
     args=vars(parser.parse_args())
 
+    # open the file to check fasta format
+    with open(args['input_file'], 'r') as file:
+        # Only need to read first line
+        line = file.readline()
+        # Verify fasta format or exit with message.
+        if not line.startswith('>'):
+            print('\n\nError reading fasta formatted read names!\n\n')
+            sys.exit('\n\nAre you sure this is a fasta formatted file?\n\n')
+
     # Run the renaming function:
-    # Print renaming file
     Fasta_rename_sequences(args['input_file'], args['naming_prefix'])
     
 if __name__ == "__main__":
